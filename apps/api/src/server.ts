6f5ba3aab26919863e0eb9f61fdfcddcf90e81d8
toddmurch <t.murchjr@gmail.com>
2024-07-22 15:19:40 -0500

    misc catchup of past work.

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,14 @@
 import { json, urlencoded } from "body-parser";
-import express, { type Express } from "express";
-import morgan from "morgan";
 import cors from "cors";
+import morgan from "morgan";
+import express, { Express } from "express";
+import session from "express-session";
+
+declare module "express-session" {
+  export interface SessionData {
+    user: { [key: string]: any };
+  }
+}
 
 export const createServer = (): Express => {
   const app = express();
@@ -11,11 +18,38 @@ export const createServer = (): Express => {
     .use(urlencoded({ extended: true }))
     .use(json())
     .use(cors())
+    .use(
+      session({
+        resave: false, // don't save session if unmodified
+        saveUninitialized: false, // don't create session until something stored
+        secret: "shhhh, very secret",
+      }),
+    )
     .get("/message/:name", (req, res) => {
       return res.json({ message: `hello ${req.params.name}` });
     })
     .get("/status", (_, res) => {
       return res.json({ ok: true });
+    })
+    .post("/login", (req, res, next) => {
+      // login logic to validate req.body.user and req.body.pass
+      // would be implemented here. for this example any combo works
+
+      // regenerate the session, which is good practice to help
+      // guard against forms of session fixation
+      req.session.regenerate(function (err: any) {
+        if (err) next(err);
+
+        // store user information in session, typically a user id
+        req.session.user = req.body.user;
+
+        // save the session before redirection to ensure page
+        // load does not happen before session is saved
+        req.session.save(function (err) {
+          if (err) return next(err);
+          res.redirect("/");
+        });
+      });
     });
 
   return app;
--- packages/eslint-config/server.js
+++ packages/eslint-config/server.js
@@ -4,6 +4,7 @@ module.exports = {
     node: true,
     es6: true,
   },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
