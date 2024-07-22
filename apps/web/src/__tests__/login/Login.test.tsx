19b368079dfac1312ae0ad24ef336a0896673882
toddmurch <t.murchjr@gmail.com>
2024-07-22 15:19:20 -0500

    feat(users-onboard): tests for login

---
+++ apps/web/src/__tests__/login/Login.test.tsx
@@ -0,0 +1,64 @@
+import LoginPage from "@/app/login/page";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+
+const mockLoginAction = vi.fn();
+vi.mock("react-dom", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("react-dom")>();
+  return {
+    ...actual,
+    __esModule: true,
+    useFormState: vi.fn(() => [{}, mockLoginAction]),
+  };
+});
+
+describe("Login Page", () => {
+  test("renders login and signup tabs", async () => {
+    render(await LoginPage());
+    expect(screen.getByTestId("Login")).toBeDefined();
+    expect(screen.getByText("Sign Up")).toBeDefined();
+  });
+
+  test("validates email format", async () => {
+    setFormValues("test", "Pawsw0rdw00f");
+    fireEvent.submit(screen.getByRole("button"));
+
+    expect(mockLoginAction).not.toHaveBeenCalled();
+  });
+
+  test("validates password length", async () => {
+    setFormValues("myemail@mydomain.com", "short");
+
+    fireEvent.submit(screen.getByRole("button"));
+
+    expect(mockLoginAction).not.toHaveBeenCalled();
+  });
+
+  test("submits form when validation succeeds", async () => {
+    const testEmail = "someemail@address.com",
+      testPassword = "Password123!";
+    setFormValues(testEmail, testPassword);
+
+    fireEvent.submit(screen.getByRole("button"));
+
+    await waitFor(() => {
+      expect(mockLoginAction).toHaveBeenCalled();
+    });
+  });
+});
+
+function setFormValues(email: string, password: string) {
+  const emailInput = screen.getByTestId("Email");
+  fireEvent.input(emailInput, {
+    target: {
+      value: email,
+    },
+  });
+
+  const passwordInput = screen.getByTestId("Password");
+  fireEvent.input(passwordInput, {
+    target: {
+      value: password,
+    },
+  });
+}
---
+++ apps/web/src/__tests__/login/Signup.test.tsx
@@ -0,0 +1,82 @@
+import { Signup } from "@/components/ux/Signup";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+
+const mockSignupAction = vi.fn();
+
+vi.mock("react-dom", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("react-dom")>();
+  return {
+    ...actual,
+    __esModule: true,
+    useFormState: vi.fn(() => [{}, mockSignupAction]),
+  };
+});
+
+describe("Signup Page", () => {
+  render(<Signup signupAction={mockSignupAction} />);
+
+  test("renders signup form", () => {
+    expect(screen.getByTestId("Email")).toBeDefined();
+    expect(screen.getByTestId("Password")).toBeDefined();
+    expect(screen.getByTestId("Confirm")).toBeDefined();
+  });
+
+  test("validates email format", async () => {
+    setFormValues("test", "Pawsw0rdw00f", "Pawsw0rdw00f");
+    fireEvent.submit(screen.getByRole("button"));
+
+    expect(mockSignupAction).not.toHaveBeenCalled();
+  });
+
+  test("validates password length", async () => {
+    setFormValues("myemail@mydomain.com", "short", "short");
+
+    fireEvent.submit(screen.getByRole("button"));
+
+    expect(mockSignupAction).not.toHaveBeenCalled();
+  });
+
+  test("validates passwords match", async () => {
+    setFormValues("someemail@address.com", "Password123!", "differentSh0rt");
+
+    fireEvent.submit(screen.getByRole("button"));
+
+    expect(mockSignupAction).not.toHaveBeenCalled();
+  });
+
+  test("submits form when validation succeeds", async () => {
+    const testEmail = "someemail@address.com",
+      testPassword = "Password123!";
+    setFormValues(testEmail, testPassword, testPassword);
+
+    fireEvent.submit(screen.getByRole("button"));
+
+    await waitFor(() => {
+      expect(mockSignupAction).toHaveBeenCalled();
+    });
+  });
+});
+
+function setFormValues(email: string, password: string, confirm: string) {
+  const emailInput = screen.getByTestId("Email");
+  fireEvent.input(emailInput, {
+    target: {
+      value: email,
+    },
+  });
+
+  const passwordInput = screen.getByTestId("Password");
+  fireEvent.input(passwordInput, {
+    target: {
+      value: password,
+    },
+  });
+
+  const confirmInput = screen.getByTestId("Confirm");
+  fireEvent.input(confirmInput, {
+    target: {
+      value: confirm,
+    },
+  });
+}
