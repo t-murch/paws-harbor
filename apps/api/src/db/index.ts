b671047da4925cdf40b69fe3e8c00df14d5c7c81
toddmurch <t.murchjr@gmail.com>
2024-12-01 09:44:18 -0600

    cleanup logs and files

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,13 +1,8 @@
+import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import { profilesTable } from './schema';
-import { config } from 'dotenv';
 
 config({ path: '.env' });
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(process.env.DATABASE_URL!, { prepare: false });
 export const db = drizzle(client);
-
-const allUsers = async () => await db.select().from(profilesTable);
-
-export { allUsers };
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,5 @@
 import { serve } from '@hono/node-server';
 import app from './server';
-import { allUsers } from './db';
 
 // const app = createServer();
 const port = process.env.PORT || 3001;
@@ -8,5 +7,4 @@ const port = process.env.PORT || 3001;
 // serve({ fetch: app.fetch, port: +port || 3001 });
 serve({ fetch: app.fetch, port: +port || 3001 });
 
-console.log(`allUsers: ${JSON.stringify(allUsers)}`);
 console.log(`listening on: ${+port || 3001}`);
