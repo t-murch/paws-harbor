dfd2c7d2e03f2a79f23c02a861daa490066b2902
toddmurch <t.murchjr@gmail.com>
2024-12-28 11:29:49 -0600

    updated scheduler to consume theme

--- apps/web/src/components/ux/Scheduler.tsx
+++ apps/web/src/components/ux/Scheduler.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Color from "color";
+import { useEffect, useState } from "react";
 import { ScheduleMeeting } from "react-schedule-meeting";
 
 export type SchedulerProps = React.ComponentProps<typeof ScheduleMeeting>;
@@ -8,11 +10,52 @@ export function Scheduler({
   availableTimeslots,
   eventDurationInMinutes,
 }: SchedulerProps) {
+  const [backgroundColor, setBackgroundColor] = useState<string>("");
+  const [primaryColor, setPrimaryColor] = useState<string>("");
+  const [borderRadius, setBorderRadius] = useState<number>(0);
+
+  useEffect(() => {
+    const rootStyles = getComputedStyle(document.documentElement);
+    const primary = rootStyles.getPropertyValue("--primary").trim().split(" ");
+    setPrimaryColor(
+      Color(`hsl(${primary[0]}, ${primary[1]}, ${primary[2]})`).hex(),
+    );
+  }, []);
+
+  useEffect(() => {
+    const rootStyles = getComputedStyle(document.documentElement);
+    const background = rootStyles
+      .getPropertyValue("--background")
+      .trim()
+      .split(" ");
+    setBackgroundColor(
+      Color(`hsl(${background[0]}, ${background[1]}, ${background[2]})`).hex(),
+    );
+  }, []);
+
+  useEffect(() => {
+    const rootStyles = getComputedStyle(document.documentElement);
+    const border =
+      Number(
+        rootStyles.getPropertyValue("--radius").trim().replace("rem", ""),
+      ) * 16;
+    setBorderRadius(border);
+  }, []);
+
+  if (!primaryColor || !backgroundColor || !borderRadius) {
+    return null;
+  }
+
   return (
-    <ScheduleMeeting
-      availableTimeslots={availableTimeslots}
-      eventDurationInMinutes={eventDurationInMinutes}
-      onStartTimeSelect={console.log}
-    />
+    <div className="schedule-wrapper">
+      <ScheduleMeeting
+        availableTimeslots={availableTimeslots}
+        borderRadius={borderRadius}
+        backgroundColor={backgroundColor}
+        eventDurationInMinutes={eventDurationInMinutes}
+        onStartTimeSelect={console.log}
+        primaryColor={primaryColor}
+      />
+    </div>
   );
 }
